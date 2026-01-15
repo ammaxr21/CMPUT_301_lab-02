069ae121d4917ac362d7087500c74f8c0d1cd655
ammaxr21 <ammaar.ayub7@gmail.com>
2026-01-15 14:57:43 -0700

    added list and delete button --> working on add city button

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+    Button addButton;
+    Button deleteButton;
+    String selectedCity = null;
+    int selectedIndex =  -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        cityList = findViewById(R.id.city_list);
+        addButton = findViewById(R.id.add_button);
+        deleteButton = findViewById(R.id.delete_button);
+
+        String []cities = {"Edmonton","Vancouver","Moscow","Sydney","Berlin","Vienna","Tokyo","Beijing","Osaka","New Delhi"};
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedCity = dataList.get(position);
+            selectedIndex = position;
+            Toast.makeText(MainActivity.this, selectedCity + " selected. Press delete to remove.", Toast.LENGTH_SHORT).show();
+        });
+
+        deleteButton.setOnClickListener( v -> {
+            if (selectedIndex > -1 && selectedCity != null) {
+                dataList.remove(selectedIndex);
+                cityAdapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, selectedCity + " has been deleted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Please select a city first", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
